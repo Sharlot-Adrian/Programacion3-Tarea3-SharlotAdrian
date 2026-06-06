16b621f092fe1325e9edccfca833cbc42022c9bd
Sharlot <sharlot_adrian@ucne.edu.do>
2026-06-06 14:46:01 -0400

    Se separo la validación de edad en dos: un caso para cuando es menor que 18 y otro caso cuando es mayor que 100 para imprimir un mensaje más especifico

--- src/RegistroUsuarios.java
+++ src/RegistroUsuarios.java
@@ -10,8 +10,10 @@ public class RegistroUsuarios {
 
         if (nombre.isEmpty() || nombre.length() < 3)
             throw new NombreInvalidoException("El nombre debe contener al menos 3 caracteres.");
-        if (edad < 18 || edad > 100)
+        if (edad < 18)
             throw new EdadInvalidadException("La edad debe ser mayor o igual a 18 años.");
+        if (edad > 100)
+            throw new EdadInvalidadException("La edad debe ser menor a 100 años.");
         if (!correo.contains("@") || !correo.contains("."))
             throw new CorreoInvalidoException("El correo electrónico no es válido. ");
         if (salario <= 0)
